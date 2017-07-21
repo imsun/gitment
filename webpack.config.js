9fdfef593a18b18d35d647f04cc49cb6144de041
GeekaholicLin <geekaholiclin@gmail.com>
2017-07-21 23:09:29 +0800

    support uglifying js

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,24 @@
 const path = require('path')
 
-module.exports = {
-  context: path.join(__dirname, 'src'),
-  entry: './gitment.js',
-  devtool: 'source-map',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'gitment.browser.js',
-    libraryTarget: 'var',
-    library: 'Gitment',
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loaders: ['babel-loader'],
-      },
-    ],
-  },
-}
+module.exports = function (env) {
+  return {
+    context: path.join(__dirname, 'src'),
+    entry: './gitment.js',
+    devtool: 'source-map',
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: (env&&env.production)?'gitment.browser.min.js':'gitment.browser.js',
+      libraryTarget: 'var',
+      library: 'Gitment',
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loaders: ['babel-loader'],
+        },
+      ],
+    },
+  }
+};
